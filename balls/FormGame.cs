d91c432db99e0ef33df98e844aceffaeb236b612
Мария <mahadrum@yandex.ru>
2023-09-12 12:12:52 +0300

    Добавьте файлы проекта.

---
+++ balls/FormGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balls
+{
+    public partial class FormGame : Form
+    {
+        FlyBall b;
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b = new FlyBall(this);
+            
+            //Ball b = new Ball();
+            //this.Controls.Add(b);
+
+            b.Show();
+        }
+
+        private void FormGame_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            FlyBall a = new FlyBall(this);    
+            a.SetPosition(e.X, e.Y);
+            a.SetRandomSpeed(-5, 5);
+            a.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.Move();
+            b.Show();
+        }
+    }
+}
---
+++ balls/ball.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balls
+{
+    class Ball:PictureBox
+    {
+        protected double x, y;
+        protected int radius = 20;
+        public Ball() { }
+        public Ball(Form owner)
+        { 
+            x = 100; y = 100;
+            this.Width = 2 * radius;
+            this.Height = 2 * radius;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Load(Environment.CurrentDirectory +"/images/red.png");
+            owner.Controls.Add(this);
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Show()
+        {
+            this.Top = (int)(y - radius);
+            this.Left = (int)(x - radius);
+        }
+    }
+
+    class FlyBall : Ball
+    {
+        double vx = 5, vy = 5;
+        Timer t;
+        
+        public FlyBall(Form owner):base(owner)
+        {
+            t = new Timer();
+            t.Interval = 10;
+            t.Tick += T_Tick;
+            t.Enabled = true;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            this.Move();
+            this.Show();
+        }
+
+        public void SetSpeed(double vx, double vy)
+        {
+            this.vx = vx;
+            this.vy = vy;
+        }
+
+        public void SetRandomSpeed(int min, int max)
+        {
+            Random rnd = new Random();
+            this.vx = rnd.Next(min, max);
+            this.vy = rnd.Next(min, max);
+        }
+
+        public void Move()
+        {
+            x = x + vx;
+            y = y + vy;
+        }
+    }
+}
